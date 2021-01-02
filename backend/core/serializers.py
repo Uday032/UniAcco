b1156f92194173a7124054c9b8a04e3a0d21a30b
Uday032 <rahul.uday07@gmail.com>
2021-01-02 13:01:03 +0530

    Login History

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -9,6 +9,22 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id','username','email', 'password')
 
 
+class UserSerializerLoginwithToken(serializers.ModelSerializer):
+
+    token = serializers.SerializerMethodField()
+
+    def get_token(self, obj):
+        jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+        jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+        payload = jwt_payload_handler(obj)
+        token = jwt_encode_handler(payload)
+        return token
+
+    class Meta:
+        model = User
+        fields = ('token','id','username','email', 'password')
+
+
 class UserSerializerWithToken(serializers.ModelSerializer):
 
     token = serializers.SerializerMethodField()
